2f76c29a2bbc20ee5e4cc5d5f78a4505be0fc185
Israel Abazie <you@example.com>
2024-07-28 04:49:19 +0100

    chore: add Morgan types in morgan.d.ts file

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 
-
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
